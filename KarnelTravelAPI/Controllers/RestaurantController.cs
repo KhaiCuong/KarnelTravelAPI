7d396291bf2089fda6aef37976782083ba1bf622
Nhat Trung <incursio40@gmail.com>
2023-05-26 19:33:39 +0700

    update fix 26/5

--- KarnelTravelAPI/Controllers/RestaurantController.cs
+++ KarnelTravelAPI/Controllers/RestaurantController.cs
@@ -1,4 +1,5 @@
-﻿using KarnelTravelAPI.Model;
+﻿using KarnelTravelAPI.CustomStatusCode;
+using KarnelTravelAPI.Model;
 using KarnelTravelAPI.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -9,42 +10,158 @@ namespace KarnelTravelAPI.Controllers
     [ApiController]
     public class RestaurantController : ControllerBase
     {
-        private IRestaurantRepository RestaurantRepository;
-        public RestaurantController(IRestaurantRepository restaurantRepository)
+        private readonly IRestaurantRepository _repository;
+        public RestaurantController(IRestaurantRepository repository)
         {
-            this.RestaurantRepository = restaurantRepository;
+            this._repository = repository;
         }
         
         [HttpGet]
-        public async Task<IEnumerable<RestaurantModel>> GetRestaurants()
+        public async Task<ActionResult<CustomResult<IEnumerable<RestaurantModel>>>> GetRestaurants()
         {
-            return await RestaurantRepository.GetRestaurants();
-            
+            try
+            {
+                var resources = await _repository.GetRestaurants();
+                if (resources != null && resources.Any())
+                {
+                    var response = new CustomResult<IEnumerable<RestaurantModel>>(200, "Resources found", resources, null);
+                    return Ok(response);
+                }
+                else
+                {
+                    var response = new CustomResult<IEnumerable<RestaurantModel>>(404, "Not Resources Found", null, null);
+                    return NotFound(response);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new CustomResult<RestaurantModel>()
+                {
+                    Message = "An error occurred while retrieving the model.",
+                    Error = ex.Message
+                });
+            }
+
         }
         
         [HttpGet("{Restaurant_id}")]
-        public async Task<RestaurantModel> GetRestaurantById(string Restaurant_id)
+        public async Task<ActionResult<CustomResult<RestaurantModel>>> GetRestaurantById(string id)
         {
-            return await RestaurantRepository.GetRestaurantById(Restaurant_id);
-            
+            try
+            {
+                var resource = await _repository.GetRestaurantById(id);
+                if (resource == null)
+                {
+                    var response = new CustomResult<RestaurantModel>(404,
+                        "Resource not found or unable to delete", null, null);
+                    return NotFound(response);
+                }
+                else
+                {
+                    var response = new CustomResult<RestaurantModel>(200,
+                        "Get employee successfully", resource, null);
+
+                    return Ok(response);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, new CustomResult<RestaurantModel>()
+                {
+                    Message = "An error occurred while retrieving the model.",
+                    Error = ex.Message
+                });
+            }
+
         }
         
         [HttpDelete("{Restaurant_id}")]
-        public async Task<bool> DeleteRestaurant(string Restaurant_id)
+        public async Task<ActionResult<CustomResult<string>>> DeleteRestaurant(string id)
         {
-            return await RestaurantRepository.DeleteRestaurant(Restaurant_id);
+            bool resourceDeleted = false;
+            var resource = await _repository.GetRestaurantById(id);
+
+            if (resource != null)
+            {
+                resourceDeleted = await _repository.DeleteRestaurant(id);
+            }
+            if (resourceDeleted)
+            {
+
+                var response = new CustomResult<string>(200,
+                    "Resource deleted successfully", null, null);
+                return Ok(response);
+            }
+            else
+            {
+                var response = new CustomResult<string>(200,
+                    "Resource not found or unable to delete", null, null);
+                return NotFound(response);
+            }
         }
         
         [HttpPost]
-        public async Task<RestaurantModel> AddRestaurant(RestaurantModel Restaurant)
+        public async Task<ActionResult<CustomResult<RestaurantModel>>> AddRestaurant
+          ([FromForm] RestaurantModel Restaurant)
         {
-            return await RestaurantRepository.AddRestaurant(Restaurant);
+            try
+            {
+                var resource = await _repository.AddRestaurant(Restaurant);
+                if (resource != null)
+                {
+                    var response = new CustomResult<RestaurantModel>(201, "Resource created",
+                        Restaurant, null);
+                    return CreatedAtAction(nameof(GetRestaurantById), new { id = Restaurant.Restaurant_id },
+                        Restaurant);
+
+                }
+                else
+                {
+                    var response = new CustomResult<RestaurantModel>(400,
+                        "Unable to create resource", null, null);
+                    return BadRequest(response);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, new CustomResult<RestaurantModel>()
+                {
+                    Message = "An error occurred while retrieving the model.",
+                    Error = ex.Message
+                });
+            }
         }
         
         [HttpPut("{Restaurant_id}")]
-        public async Task<RestaurantModel> UpdateRestaurant(RestaurantModel Restaurant)
+        public async Task<ActionResult<CustomResult<RestaurantModel>>> UpdateRestaurant
+         ([FromForm] RestaurantModel Restaurant)
         {
-            return await RestaurantRepository.UpdateRestaurant(Restaurant);
+            try
+            {
+                var resource = await _repository.UpdateRestaurant(Restaurant);
+                if (resource != null)
+                {
+                    var response = new CustomResult<RestaurantModel>(200,
+                        "update employee successfully", Restaurant, null);
+                    return Ok(response);
+                }
+                else
+                {
+                    var response = new CustomResult<RestaurantModel>(404,
+                        "No employee to update", null, null);
+                    return NotFound(response);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new CustomResult<RestaurantModel>()
+                {
+                    Message = "An error occurred while retrieving the model.",
+                    Error = ex.Message
+                });
+            }
         }
     }
 }
--- KarnelTravelAPI/Service/RestaurantServiceImp.cs
+++ KarnelTravelAPI/Service/RestaurantServiceImp.cs
@@ -3,59 +3,44 @@ using KarnelTravelAPI.Model.ImageModel;
 using KarnelTravelAPI.Repository;
 using KarnelTravelAPI.Repository.ImageRepository;
 using Microsoft.EntityFrameworkCore;
+using System.Net.Http.Headers;
 
 namespace KarnelTravelAPI.Service
 {
     public class RestaurantServiceImp : IRestaurantRepository
     {
-        /*private  string _uploadFolder;*/
-        private readonly DatabaseContext context;
-        public RestaurantServiceImp(DatabaseContext context)
-        {
-            this.context = context;
-        }
+        private readonly DatabaseContext _databaseContext;
+       /* private readonly ITouristSpotImageRepository _touristSpotImage;*/
 
-        public async Task<RestaurantModel> AddRestaurant(RestaurantModel Restaurant, ICollection<IFormFile> files)
+        public RestaurantServiceImp(DatabaseContext databaseContext)
         {
-            if (Restaurant != null)
+            _databaseContext = databaseContext;
+        }
+        public async Task<RestaurantModel> AddRestaurant(RestaurantModel restaurant)
+        {
+            var res = await _databaseContext.Restaurants.FirstOrDefaultAsync(r => r.Restaurant_id.Equals(restaurant.Restaurant_id));
+            if (res == null)
             {
-                await context.Restaurants.AddAsync(Restaurant);
-                await context.SaveChangesAsync();
-                if (files.Count > 0)
-                {
-                    foreach (var file in files)
-                    {
-                        if (file.Length > 0)
-                        {
-                            var fileName = Path.GetRandomFileName() + Path.GetFileName(file.FileName);
-                            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", fileName);
-                            using (var fileStream = new FileStream(filePath, FileMode.Create))
-                            {
-                                await file.CopyToAsync(fileStream);
-                            }
-                            var image = new RestaurantImageModel
-                            {
-                                photo_url = "/images/" + fileName,
-                                Restaurant_id = Restaurant.Restaurant_id,
-                            };
-                            context.RestaurantImages.Add(image);
-                        }
-                        
-                    }
-                }
-            }
-            return Restaurant;
-        }
+                await _databaseContext.Restaurants.AddAsync(restaurant);
+                await _databaseContext.SaveChangesAsync();
+                return restaurant;
 
-       
+            }
+            else
+            {
+                return null;
+            }
+        }
 
         public async Task<bool> DeleteRestaurant(string Restaurant_id)
         {
-            RestaurantModel restaurant = await context.Restaurants.FindAsync(Restaurant_id);
-            if (restaurant != null)
+            RestaurantModel res = await _databaseContext.Restaurants.FindAsync(Restaurant_id);
+
+
+            if (res != null)
             {
-                context.Restaurants.Remove(restaurant);
-                await context.SaveChangesAsync();
+                _databaseContext.Restaurants.Remove(res);
+                await _databaseContext.SaveChangesAsync();
                 return true;
             }
             else
@@ -65,11 +50,11 @@ namespace KarnelTravelAPI.Service
         }
 
         public async Task<RestaurantModel> GetRestaurantById(string Restaurant_id)
-        {
-            RestaurantModel restaurant = await context.Restaurants.FindAsync(Restaurant_id);
-            if (restaurant != null)
+        {            RestaurantModel res = await _databaseContext.Restaurants.FindAsync(Restaurant_id);
+            if (res != null)
             {
-                return restaurant;
+
+                return res;
             }
             else
             {
@@ -79,18 +64,16 @@ namespace KarnelTravelAPI.Service
 
         public async Task<IEnumerable<RestaurantModel>> GetRestaurants()
         {
-            return await context.Restaurants.ToListAsync();
-            
+            return await _databaseContext.Restaurants.ToListAsync();
         }
 
         public async Task<RestaurantModel> UpdateRestaurant(RestaurantModel Restaurant)
         {
-            RestaurantModel res = await context.Restaurants.FindAsync(Restaurant.Restaurant_id);
+            RestaurantModel res = await _databaseContext.Restaurants.FirstOrDefaultAsync(r => r.Restaurant_id.Equals(Restaurant.Restaurant_id));
             if (res != null)
             {
-                res.Restaurant_name = Restaurant.Restaurant_name;
-                res.Price = Restaurant.Price;
-                await context.SaveChangesAsync();
+                _databaseContext.Entry(Restaurant).State = EntityState.Modified;
+                await _databaseContext.SaveChangesAsync();
                 return res;
             }
             else
@@ -98,26 +81,5 @@ namespace KarnelTravelAPI.Service
                 return null;
             }
         }
-
-        /*public RestaurantServiceImp(IWebHostEnvironment webHostEnvironment)
-        {
-            _uploadFolder = Path.Combine(webHostEnvironment.ContentRootPath, "uploads");//muốn đưa vào folder nào thì ghi tên folder đó
-            if (!Directory.Exists(_uploadFolder))
-            {
-                Directory.CreateDirectory(_uploadFolder);
-            }
-        }
-
-        public async Task<string> UploadFile(IFormFile file)
-        {
-            string fileName = Path.GetFileName(file.FileName);
-            string filePath = Path.Combine(_uploadFolder, fileName);// từ uploadfolder có thể đi đến fileName
-            using (var stream = new FileStream(filePath, FileMode.Create))
-            {
-                await file.CopyToAsync(stream);
-            }
-            return fileName;
-        }*/
-
     }
 }
